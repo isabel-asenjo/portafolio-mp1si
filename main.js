5506353085dccea1d90d1a9b69ccc6c132cdc30a
Isabel <isabel.asenjo@correo.unimet.edu.ve>
2020-10-18 16:10:33 -0400

    hero1 js

---
+++ main.js
@@ -0,0 +1,44 @@
+document.getElementById("navbar-links-toHome").addEventListener("click", function(){alert("x");});
+
+
+
+document.getElementsByClassName("next").addEventListener("click", function(){alert("y");});
+
+
+
+document.getElementsByClassName("prev").addEventListener("click", cambiarSlides(-1));
+document.getElementsByClassName("next").addEventListener("click", cambiarSlides(1));
+
+
+
+var indiceSlideshow = 1;
+mostrar(indicesSlideshow);
+
+
+function mostrar(indice){
+    var fotos = document.getElementsByClassName("carouselImages");
+    var x;
+    
+    if (indice > fotos.length){
+        indicesSlideshow = 1; /*esto significa que dio la vuelta completa*/
+    }
+    if (indice < 1) {
+        indiceSlideshow = fotos.length; /*esto significa que dio la vuelta pero al revés*/
+    }
+    for (x=0; x < fotos.length; x++){
+        fotos[x].style.display = "none";
+    }
+
+    fotos[indiceSlideshow -1].style.display = "block";
+    
+    
+}
+
+
+function slideActual(indice){
+    mostrar(indiceSlideshow = indice)
+}
+
+function cambiarSlides(indice){
+    mostrar(indiceSlideshow += indice)
+}
